44db7a501f01856485dc2a3cf59c26ccdafb2731
maurovolpe18 <maurovolpe18@gmail.com>
2020-10-13 12:25:09 -0300

    Variable de enterno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,8 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://mauro:RQUf5zYerJVCwpvW@cluster0.juxox.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
