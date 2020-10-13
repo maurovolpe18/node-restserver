123a18ed86afe025139eda0dde4f77e5826d158d
maurovolpe18 <maurovolpe18@gmail.com>
2020-10-13 12:13:55 -0300

    config arreglo

--- server/config/config.js
+++ server/config/config.js
@@ -16,10 +16,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 let urlDB;
 
-//if (process.env.NODE_ENV === "dev") {
-//urlDB = "mongodb://localhost:27017/cafe";
-//} else {
-urlDB = "mongodb+srv://mauro:RQUf5zYerJVCwpvW@cluster0.juxox.mongodb.net/cafe";
-//}
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
+} else {
+  urlDB =
+    "mongodb+srv://mauro:RQUf5zYerJVCwpvW@cluster0.juxox.mongodb.net/cafe";
+}
 
 process.env.URLDB = urlDB;
