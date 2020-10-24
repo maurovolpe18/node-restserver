982ad95c46bd5ccc7d977d14a531809a8c05a682
maurovolpe18 <maurovolpe18@gmail.com>
2020-10-24 15:24:47 -0300

    Carga de Imagenes

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -40,7 +40,30 @@ let verificaAdmin_Role = (req, res, next) => {
     });
   }
 };
+
+// ==========================
+//  VERIFICAR TOKEN IMG
+// ==========================
+
+let verificaTokenImg = (req, res, next) => {
+  let token = req.query.token;
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err: {
+          message: "token no valido",
+        },
+      });
+    }
+    req.usuario = decoded.usuario;
+
+    next();
+  });
+};
 module.exports = {
   verificaToken,
   verificaAdmin_Role,
+  verificaTokenImg,
 };
